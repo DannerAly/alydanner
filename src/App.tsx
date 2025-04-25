1f553b9906ca917f1924fbc78d5a4903ab5b7f89
DannerAly <dannerabelaly@gmail.com>
2025-04-25 19:18:36 -0400

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import "./App.css";
+
+import Particles from "./components/fondo/Particles";
+import Presentacion from "./components/presentacion";
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#042F40",
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={100}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={true}
+          disableRotation={false}
+        />
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <Presentacion />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/presentacion.tsx
@@ -0,0 +1,33 @@
+import TiltedCard from './Imagen';
+export default function Presentacion() {
+    return (
+
+        <div className="flex flex-raw items-center justify-center h-screen bg-gradient-to-b from-[#042F40] to-[#042F40]">
+            <TiltedCard
+                imageSrc="src\assets\images\perfil IN2.png"
+                altText="Aly Danner"
+                captionText="Aly Danner"
+                containerHeight="300px"
+                containerWidth="300px"
+                imageHeight="300px"
+                imageWidth="300px"
+                rotateAmplitude={12}
+                scaleOnHover={1.2}
+                showMobileWarning={false}
+                showTooltip={true}
+                displayOverlayContent={true}
+                overlayContent={
+                    <p className="tilted-card-demo-text">
+                    </p>
+                }
+            />
+            <div className="text-center text-white mt-4">
+                <h1 className="text-2xl font-bold">Hola, soy Aly Danner</h1>
+                <p className="text-lg">Desarrollador Web/Mobile & Productor Audiovisual</p>
+                
+
+            </div>
+        </div>
+
+    );
+}
