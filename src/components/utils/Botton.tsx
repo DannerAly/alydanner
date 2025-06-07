f0174ddf1373fad968191cc3da44437752c8cfb5
DannerAly <dannerabelaly@gmail.com>
2025-06-06 23:14:47 -0400

    Bio y frase 8

--- src/components/utils/Botton.tsx
+++ src/components/utils/Botton.tsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 function Button({ to, children }: { to: string, children: React.ReactNode }) {
     return (
-        <> 
-        
+        <>
+
             <Link
                 to={to}
-                className="bg-gray-200 text-zinc-800 text-xs md:text-base 
-                rounded-xl py-2 px-4 shadow-lg inline-flex items-center justify-center 
-                hover:bg-[#12F2F2]  hover:text-gray-200 font-bold transition-colors"
+                className="bg-white border-2 border-cyan-400 text-zinc-800 text-xs md:text-base
+    rounded-xl py-2 px-6 shadow-xl shadow-cyan-400/30 inline-flex items-center justify-center
+    hover:bg-cyan-400 hover:text-white hover:shadow-cyan-400/60 font-bold transition-all duration-200
+    focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2"
             >
                 {children}
             </Link>
-        
+
         </>
 
     );
